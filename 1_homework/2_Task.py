06f2d377d2dd2033e559dd7336c6df049e028b15
Arthur <ar3tyr7@gmail.com>
2022-11-25 12:38:01 +0300

    fix

--- 1_homework/2_Task.py
+++ 1_homework/2_Task.py
@@ -1,10 +1,10 @@
 # Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
-x = (False,True)
-y = (False,True)
-z = (False,True)
+x = [False,True]
+y = [False,True]
+z = [False,True]
 for i in x:
     for j in y:
         for k in z:
-            print(not (x or y or z) == (not x) and (not y) and (not z))
+            print((not (i or j or k)) == ((not i) and (not j) and (not k)))
