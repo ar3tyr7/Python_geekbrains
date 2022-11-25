504b7c6e5a71ac0a5eda18490f5dc9eee60f4f87
Arthur <ar3tyr7@gmail.com>
2022-11-25 12:39:58 +0300

    fix2

--- 1_homework/2_Task.py
+++ 1_homework/2_Task.py
@@ -1,10 +1,8 @@
 # Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
-x = [False,True]
-y = [False,True]
-z = [False,True]
-for i in x:
-    for j in y:
-        for k in z:
-            print((not (i or j or k)) == ((not i) and (not j) and (not k)))
+arrayBool = [False,True]
+for x in arrayBool:
+    for y in arrayBool:
+        for z in arrayBool:
+            print((not (x or y or z)) == ((not x) and (not y) and (not z)))
